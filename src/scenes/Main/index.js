c8ddcdefd94279446795a0cdcdab7cd0c4edf6dd
unknown <13517145@std.stei.itb.ac.id>
2020-09-06 22:09:47 +0700

    added affine cipher

--- src/scenes/Main/index.js
+++ src/scenes/Main/index.js
@@ -18,7 +18,11 @@ class Main extends React.Component {
       encryptedText:'',
       decryptedText:'',
       alphabetConfig:alphabet,
-      encryptionMode:true
+      encryptionMode:true,
+      a:'',
+      b:'',
+      encryptionMatrix:'',
+      decryptionMatrix:''
     }
   }
   encryptionType = [
@@ -98,6 +102,18 @@ class Main extends React.Component {
     })
   }
 
+  handleAChange = event => {
+    this.setState({
+      a:event.target.value
+    })
+  }
+
+  handleBChange = event => {
+    this.setState({
+      b:event.target.value
+    })
+  }
+
   shuffleAlphabetConfig = () => {
     const { alphabetConfig } = this.state
     let shuffledAlphabet = alphabetConfig.sort(() => Math.random() - 0.5)
@@ -127,8 +143,6 @@ class Main extends React.Component {
         tempKey = key.toUpperCase().trim()
         for (let i = 0; i < temp.length; i++){
           temp[i] = alphabetConfig[((alphabetConfig.indexOf(temp[i]) + alphabetConfig.indexOf(tempKey[i % tempKey.length])) % 26)]
-          console.log(((alphabetConfig.indexOf(temp[i]) + alphabetConfig.indexOf(tempKey[i])) % 26))
-          console.log("---------------------------")
         }
         temp = temp.join('')
         this.setState({
@@ -148,11 +162,11 @@ class Main extends React.Component {
         })
         break
       case 'Extended Vigenere':
-        
         temp = plainText.replace(/ /g,'').trim().toUpperCase().split('')
         tempKey = key.toUpperCase().trim()
         for (let i = 0; i < temp.length; i++){
-          temp[i] = String.fromCharCode((((temp[i].charCodeAt(0)) + (tempKey[i % tempKey.length].charCodeAt(0))) % 256))        }
+          temp[i] = String.fromCharCode((((temp[i].charCodeAt(0)) + (tempKey[i % tempKey.length].charCodeAt(0))) % 256))        
+        }
         temp = temp.join('')
         this.setState({
           encryptedText: temp
@@ -165,6 +179,19 @@ class Main extends React.Component {
   
         break
       case 'Affine':
+        const { a, b } = this.state
+        if (parseInt(a) % 2 === 0 || parseInt(a) % 13 === 0 || parseInt(a) % 26 === 0){
+          console.log("Invalid number!")
+        } else {
+          temp = plainText.replace(/ /g, '').trim().toUpperCase().split('')
+          for (let i = 0; i < temp.length; i++) {
+            temp[i] = String.fromCharCode((((temp[i].charCodeAt(0) - 65)*parseInt(a) + parseInt(b)) % 26) + 65)
+          }
+          temp = temp.join('')
+          this.setState({
+            encryptedText: temp
+          })
+        }
         break
       case 'Hill':
         break    
@@ -236,8 +263,26 @@ class Main extends React.Component {
   
         break
       case 'Affine':
+        const { a, b } = this.state
+        if (parseInt(a) % 2 === 0 || parseInt(a) % 13 === 0 || parseInt(a) % 26 === 0){
+          console.log("Invalid number!")
+        } else {
+          let inverseModulo = 2
+          while (inverseModulo*a % 26 !== 1){
+            inverseModulo++
+          }
+          temp = encryptedText.replace(/ /g, '').trim().toUpperCase().split('')
+          for (let i = 0; i < temp.length; i++) {
+            temp[i] = String.fromCharCode(((((((temp[i].charCodeAt(0) - 65)-parseInt(b)) * parseInt(inverseModulo)) % 26) + 26) % 26) + 65)
+          }
+          temp = temp.join('')
+          this.setState({
+            decryptedText: temp
+          })
+        }
         break
       case 'Hill':
+
         break   
       default:
         console.log("Error!")
@@ -255,6 +300,30 @@ class Main extends React.Component {
         <Button onClick={this.shuffleAlphabetConfig}>
           Randomize
         </Button>
+      </Grid>
+    ) : null
+  }
+
+  AffineUtils = () => {
+    const { b, a, cipher } = this.state
+    return cipher === 'Affine' ? (
+      <Grid container direction="row">
+        <TextField style={{width:"75%"}} label="Enter your a..." value={a} onChange={this.handleAChange}>
+        </TextField>
+        <TextField style={{width:"75%"}} label="Enter your shift" value={b} onChange={this.handleBChange}>
+        </TextField>
+      </Grid>
+    ) : null
+  }
+
+  SuperUtils = () => {
+    const { shift, partition, cipher } = this.state
+    return cipher === 'Super' ? (
+      <Grid container direction="row">
+        <TextField style={{width:"75%"}} label="Enter your shift..." value={a} onChange={this.handleAChange}>
+        </TextField>
+        <TextField style={{width:"75%"}} label="Enter your partition..." value={b} onChange={this.handleBChange}>
+        </TextField>
       </Grid>
     ) : null
   }
@@ -288,9 +357,10 @@ class Main extends React.Component {
           </Grid>
           <Grid item xs={12}>
             <this.AlphabetConfig/>
+            <this.AffineUtils/>
           </Grid>
           <Grid item xs={12}>  
-            <TextField onChange={this.handleKeyChange} value={key} style={{width:"100%"}} label="Enter key here..."></TextField>
+            {cipher !== "Affine" ? (<TextField onChange={this.handleKeyChange} value={key} style={{width:"100%"}} label="Enter key here..."></TextField>) : null}
           </Grid>
           <Grid item xs={12}>
             <TextField value={encryptionMode ? encryptedText : decryptedText}style={{width:"100%"}} label={encryptionMode ? "Encrypted result..." : "Decryption result..."} disabled></TextField>
